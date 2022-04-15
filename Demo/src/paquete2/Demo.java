afef01d872d9d36bb7395dc11df849d012d004a9
CarlosMC2003 <carlitosenrike@gmail.com>
2022-04-14 22:30:13 -0500

    Taller00

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -1,23 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
 
-
-/**
- *
- * @author reroes
- */
+import paquete1.Mensaje;
+import paquete3.Valor;
 public class Demo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        System.out.printf();
+        System.out.printf("%s con %d %n", Mensaje.obtenerFrase(), Valor.obtenerHabitantes());
     }
     
-}
+} 
